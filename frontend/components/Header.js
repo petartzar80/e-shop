33195460e7a1cd11422c228a28379f86ecdcc0ac
tzar <petartzar80@gmail.com>
2020-07-07 09:12:32 +0200

    applied basic styling to Page & Header

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,16 +1,60 @@
+import Link from 'next/link';
+import styled from 'styled-components';
+
 import Nav from './Nav';
 
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 0.4rem;
+  position: relative;
+  z-index: 2;
+  line-height: 1;
+  a {
+    color: black;
+    text-transform: uppercase;
+    text-decoration: none;
+  }
+  @media (max-width: 1300px) {
+    margin: 0;
+    text-align: center;
+  }
+  margin-top: 0;
+  margin-bottom: 0;
+`;
+
+const StyledHeader = styled.header`
+  .bar {
+    padding: 1rem;
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: stretch;
+    @media (max-width: 1300px) {
+      grid-template-columns: 1fr;
+      justify-content: center;
+    }
+  }
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+  }
+`;
+
 const Header = () => (
-  <div>
+  <StyledHeader>
     <div className="bar">
-      <a href="">TRANSISTORE</a>
+      <Logo>
+        <Link href="/">
+          <a>Transistore</a>
+        </Link>
+      </Logo>
       <Nav />
     </div>
     <div className="sub-bar">
       <p>Search</p>
     </div>
     <div>Cart</div>
-  </div>
+  </StyledHeader>
 );
 
 export default Header;
--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,14 +1,37 @@
+import styled, { ThemeProvider } from 'styled-components';
+
 import Header from './Header';
 import Meta from './Meta';
 
+const theme = {
+  black: '#000000',
+  white: '#FFFFFF',
+  lemon: '#f9ed43',
+  lyon: '#1c4386',
+  maxWidth: '1000px',
+  bs: 'none',
+};
+
+const StyledPage = styled.div`
+  background: ${props => props.theme.white};
+  color: ${props => props.theme.black};
+`;
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
 const Page = props => {
   return (
-    <div>
-      <Meta />
-      <Header />
-      <p>Page component</p>
-      {props.children}
-    </div>
+    <ThemeProvider theme={theme}>
+      <StyledPage>
+        <Meta />
+        <Header />
+        <Inner>{props.children}</Inner>
+      </StyledPage>
+    </ThemeProvider>
   );
 };
 
